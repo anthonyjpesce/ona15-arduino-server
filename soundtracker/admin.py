2d107d277534ce5680e12cfc8e964dfdac9e7c3e
Anthony Pesce <anthonyjpesce@gmail.com>
2015-09-13 12:05:32 -0700

    simple admin and wsgi tweak

--- soundtracker/admin.py
+++ soundtracker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from soundtracker.models import Signal
 
-# Register your models here.
+
+class SignalAdmin(admin.ModelAdmin):
+    list_display = ['timestamp', 'arduino_number', 'voltage']
+    list_filter = ['arduino_number']
+
+admin.site.register(Signal, SignalAdmin)
