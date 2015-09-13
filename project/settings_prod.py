3ca264ae289f55aa41e2330b3f69b5a628ac7917
Anthony Pesce <anthonyjpesce@gmail.com>
2015-09-12 23:41:51 -0700

    inital commit from the datadesk project template

---
+++ project/settings_prod.py
@@ -0,0 +1,23 @@
+DEBUG = False
+DEVELOPMENT, PRODUCTION = False, True
+DEBUG_TOOLBAR = False
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': 60 * 30,
+        'OPTIONS': {
+            'MAX_ENTRIES': 1500
+        }
+    }
+}
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
+STATIC_URL = ''
+WSGI_APPLICATION = 'project.wsgi_prod.application'
+ALLOWED_HOSTS = ()
