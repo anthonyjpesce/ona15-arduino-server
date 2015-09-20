09b2a822c7cd19c5c5c2ecf96766d9cdafbd1902
emamd <armand.emamdjomeh@latimes.com>
2015-09-20 14:15:48 -0700

    named robots and better generated HTML

--- soundtracker/views.py
+++ soundtracker/views.py
@@ -77,7 +77,6 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-
-        context["voltages"] = Signal.objects.all().values_list('voltage')
+        context['robots'] = Robot.objects.all()
 
         return context
