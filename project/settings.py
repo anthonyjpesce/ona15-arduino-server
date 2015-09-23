4547ca6098543247cf3670def7940dbc5470a838
anthonyjpesce <anthonyjpesce@gmail.com>
2015-09-22 18:00:41 -0700

    added static url

--- project/settings.py
+++ project/settings.py
@@ -16,6 +16,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+STATIC_URL = "https://s3-us-west-2.amazonaws.com/ona15bot/static/"
+
 # Media and static files
 MEDIA_ROOT = os.path.join(BASE_DIR, '.media')
 STATIC_ROOT = os.path.join(BASE_DIR, '.static')
